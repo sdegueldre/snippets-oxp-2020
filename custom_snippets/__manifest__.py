f7b194d35f5afd67e9e71938762c3078da492e5d
Nathan Bangwa <nathanbangwa7@gmail.com>
2021-12-23 16:09:22 +0200

    put all assets refs on __manifest__.py (odoo 15 doc)

--- custom_snippets/__manifest__.py
+++ custom_snippets/__manifest__.py
@@ -5,12 +5,24 @@
     'summary': 'Adds custom snippets website',
     'version': '1.0',
     'description': "",
-    'depends': ['website_sale'],
+    'depends': ['website', 'website_sale', 'web'],
     'qweb': ['static/src/xml/*.xml'],
     'data': [
         'views/snippets/snippets.xml',
         'views/snippets/s_cart_products.xml',
     ],
+
+    # assets
+    'assets':{
+        "web.assets_frontend":[
+            "/custom_snippets/static/src/snippets/s_cart_products/000.js",
+        ],
+
+        "web.assets_editor":[
+            "/custom_snippets/static/src/snippets/s_cart_products/options.js",
+        ],
+    },
+
     'installable': True,
     'application': True,
 }
